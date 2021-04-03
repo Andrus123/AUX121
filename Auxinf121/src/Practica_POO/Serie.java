abbad3d0f33404e6802aeb1b28b0a1b2f2740a70
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-03 01:52:49 -0400

    Serie

--- Auxinf121/src/Practica_POO/Serie.java
+++ Auxinf121/src/Practica_POO/Serie.java
@@ -5,6 +5,8 @@
  */
 package Practica_POO;
 
+import clase1.Leer;
+
 /**
  *
  * @author andru
@@ -16,4 +18,68 @@ public class Serie {
     private int nroTemporadas;
     private String[][] Temporada = new String[3][10];
     
+    public Serie(){
+        this.titulo = "Dark";
+        this.genero = "Suspenso";
+        this.creador = "Baran bo Odar";
+        this.nroTemporadas = 3;
+        this.Temporada[0][0] = "1";        this.Temporada[0][1] = "2";       this.Temporada[0][2] = "3";
+        this.Temporada[1][0] = "Secretos"; this.Temporada[1][1] = "Mentiras";this.Temporada[1][2] = "Pasado y Presente";
+        this.Temporada[2][0] = "10";       this.Temporada[2][1] = "8";       this.Temporada[2][2] = "8";
+    }
+
+    public Serie(String titulo, String creador) {
+        this.titulo = titulo;
+        this.creador = creador;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
+
+    public int getNroTemporadas() {
+        return nroTemporadas;
+    }
+
+    public void setNroTemporadas(int nroTemporadas) {
+        this.nroTemporadas = nroTemporadas;
+    }
+
+    public String[][] getTemporada() {
+        return Temporada;
+    }
+
+    public void setTemporada(String[][] Temporada) {
+        this.Temporada = Temporada;
+    }
+    public void leer(){
+        System.out.println("Titulo de la serie:");
+        this.titulo = Leer.dato();
+        System.out.println("Género de la serie:");
+        this.genero = Leer.dato();
+        System.out.println("Creador de la serie:");
+        this.creador = Leer.dato();
+        System.out.println("Numero de temporadas:");
+        this.nroTemporadas = Leer.datoInt();
+    }
 }
