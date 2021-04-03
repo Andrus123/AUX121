9f58ad3f1aac85f384b287a3f6318a0404b77897
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-03 01:40:58 -0400

    Serie

---
+++ Auxinf121/src/Practica_POO/Serie.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica_POO;
+
+/**
+ *
+ * @author andru
+ */
+public class Serie {
+    private String titulo;
+    private String genero;
+    private String creador;
+    private int nroTemporadas;
+    private String[][] Temporada = new String[3][10];
+    
+}
