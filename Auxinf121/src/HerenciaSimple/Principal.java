8deb005050b0f090f1b5111a6083d2aac50ae5bd
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-10 23:33:43 -0400

    Herencia Simple.

--- Auxinf121/src/HerenciaSimple/Principal.java
+++ Auxinf121/src/HerenciaSimple/Principal.java
@@ -11,4 +11,5 @@ package HerenciaSimple;
  */
 public class Principal {
     
+    
 }
--- Auxinf121/src/HerenciaSimple/Ropero.java
+++ Auxinf121/src/HerenciaSimple/Ropero.java
@@ -13,5 +13,30 @@ public class Ropero {
     private int nroCajas;
     private Double ancho;
     private Double alto;
+
+    public int getNroCajas() {
+        return nroCajas;
+    }
+
+    public void setNroCajas(int nroCajas) {
+        this.nroCajas = nroCajas;
+    }
+
+    public Double getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(Double ancho) {
+        this.ancho = ancho;
+    }
+
+    public Double getAlto() {
+        return alto;
+    }
+
+    public void setAlto(Double alto) {
+        this.alto = alto;
+    }
+    
     
 }
