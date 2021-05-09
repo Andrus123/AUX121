3f713d4c5627ccca58da52d1e853070c274f2e4d
Andrés K. Aquin <andruuus123@gmail.com>
2021-05-09 01:44:24 -0400

    Ejercicio de Composición: Serie, inciso c) Mostrar el titulo del episodio X de la serie Y

--- Auxinf121/src/Serie/Principal.java
+++ Auxinf121/src/Serie/Principal.java
@@ -10,7 +10,29 @@ package Serie;
  * @author Andrés Aquin
  */
 public class Principal {
-    public static void main(String[] args){
+
+    public static void verepisodio(String nom, Serie x, Serie y) {
+        for (int i = 0; i < x.getNrotemporadas(); i++) {
+            if (x.equals(x.temporada[i].episodio[i].getTitulo())) {
+                System.out.println("Episodio: "+
+                        x.temporada[i].episodio[i].getTitulo()
+                        + " temp: "+x.temporada[i].getNrotemporada());
+            }else{
+                System.out.println("Episodio no encontrado en serie: "+x.getNombre());
+            }
+        }
+        for (int i = 0; i < y.getNrotemporadas(); i++) {
+            if (x.equals(y.temporada[i].episodio[i].getTitulo())) {
+                System.out.println("Episodio: "+
+                        y.temporada[i].episodio[i].getTitulo()
+                        + " temp: "+y.temporada[i].getNrotemporada());
+            }else{
+                System.out.println("Episodio no encontrado en serie: "+y.getNombre());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         //a) Instanciar dos Series
         Serie s1 = new Serie();
         Serie s2 = new Serie();
@@ -23,6 +45,15 @@ public class Principal {
         //c) Mostrar el titulo del episodio X de la serie Y
         s1.mostrarepisodio();
         s2.mostrarepisodio();
-        //d)
+        //d) Mostrar la serie con más temporadas
+        if (s1.getNrotemporadas() > s2.getNrotemporadas()) {
+            System.out.println("Serie con más temporadas: " + s1.getNombre()
+                    + " con " + s1.getNrotemporadas() + " temporadas");
+        } else {
+            System.out.println("Serie con más temporadas: " + s2.getNombre()
+                    + " con " + s2.getNrotemporadas() + " temporadas");
+        }
+        //e) Verificar si existe el episodio X
+        verepisodio("Ozymandias", s1, s2);
     }
 }
